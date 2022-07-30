2fe5f2960a26eba04aa0aa2e159d73a3ef395104
Jeon su-jin <81217456+soozynn@users.noreply.github.com>
2022-07-30 21:49:24 +0900

    Revert "요구사항 완료"
    This reverts commit b71a2f80feab225213b66261a0437a47995ac6e3.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,60 +1,11 @@
 import React, { useEffect } from "react";
-import { createGlobalStyle } from "styled-components";
-import styled from "styled-components";
-import { v4 as uuidv4 } from "uuid";
-
-import { useAppSelector, useAppDispatch } from "../store/hooks";
-import { fetchIssuesAsync, Issue } from "../features/issues/issuesSlice";
-import IssueBox from "../components/IssueBox";
-
-const GlobalStyles = createGlobalStyle`
-  body, html {
-    margin: 0;
-    height: 100%;
-    width: 100%;
-    font-family: "Acme", sans-serif;
-  }
-
-  li {
-    list-style: none;
-  }
-
-  :root {
-    --color-red: #FF0000;
-    --color-black: #000000;
-  }
-`;
-
-const ErrorMessage = styled.div`
-  color: var(--color-red);
-`;
 
 export default function App() {
-  const issues = useAppSelector((state) => state.issues);
-  const dispatch = useAppDispatch();
-
   useEffect(() => {
-    dispatch(fetchIssuesAsync());
-  }, [dispatch]);
+    fetch("https://api.github.com/repos/facebook/create-react-app/issues")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
 
-  return (
-    <>
-      <GlobalStyles />
-      {issues.isLoading && <div>Loading...</div>}
-      {issues.isError && <ErrorMessage>{issues.error}</ErrorMessage>}
-      {!issues.isLoading && issues.issues.length ? (
-        <ul>
-          {issues.issues
-            .slice()
-            .sort(
-              (a: Issue, b: Issue): number =>
-                Number(b.comments) - Number(a.comments)
-            )
-            .map((issue) => (
-              <IssueBox key={uuidv4()} data={issue} />
-            ))}
-        </ul>
-      ) : null}
-    </>
-  );
+  return <div className="App"></div>;
 }

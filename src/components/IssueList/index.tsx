42faed0cd5731dd80a7728fcb9334ef09e371ac3
Jeon su-jin <81217456+soozynn@users.noreply.github.com>
2022-07-30 21:57:12 +0900

    Revert "Revert "요구사항 완료""
    This reverts commit 28d4285d197aba45f0583032f3c2b6f44a3cec88.

--- src/components/IssueList/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function IssueList() {
-  return <div></div>;
-}
---
+++ src/features/issues/issuesSlice.ts
@@ -0,0 +1,81 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export interface Issue {
+  number: string;
+  title: string;
+  created_at: string;
+  comments: string;
+  photo_url: string;
+}
+
+interface IssueListState {
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+  error: string | null;
+  issues: Issue[];
+}
+
+interface ServerError {
+  statusCode: number;
+  description: string;
+}
+
+const initialState: IssueListState = {
+  isLoading: false,
+  isSuccess: false,
+  isError: false,
+  error: "",
+  issues: [],
+};
+
+export const fetchIssuesAsync = createAsyncThunk<
+  Issue[],
+  void,
+  {
+    rejectValue: ServerError;
+  }
+>("issues/fetch", async (_, { rejectWithValue }) => {
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/facebook/create-react-app/issues`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return rejectWithValue(error as ServerError);
+  }
+});
+
+export const issuesSlice = createSlice({
+  name: "issues",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchIssuesAsync.pending, (state) => {
+        state.isLoading = true;
+        state.isSuccess = false;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(fetchIssuesAsync.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.isError = false;
+        // 걸러주기
+        state.issues.push(...payload);
+        state.error = null;
+      })
+      .addCase(fetchIssuesAsync.rejected, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = false;
+        state.isError = true;
+        state.issues = [];
+        // state.error = payload || "Error: Something went wrong";
+      });
+  },
+});
+
+export default issuesSlice.reducer;
